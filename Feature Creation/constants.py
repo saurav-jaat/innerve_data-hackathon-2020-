bb9f19ac49582ba265d182ccd9cf7b9225b42581
Deepak Sahu <deepak@iiitkalyani.ac.in>
2020-10-22 18:43:57 +0530

    Feature Creation

---
+++ Feature Creation/constants.py
@@ -0,0 +1,11 @@
+#--------------------------------------
+# CONSTANTS FOR THE OTHER FILES
+#--------------------------------------
+#
+# PATH TO DATA
+DATA_PATH = '../../data_v2/'
+RANDOM_STATE = 28
+#
+# output file name
+OUTPUT_FILE = 'train_short_features.csv'
+#************************************
---
+++ Feature Creation/readme.txt
@@ -0,0 +1,6 @@
+#  FOLDER DESCRIPTION
+All the files in this folder create a feature 
+using the data and output in
+`train_short_features.csv`
+
+# Change Folder Directory in settings
